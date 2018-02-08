a738d2d5a0dce50f180c5bc40ac388f7bf381ee7
matthew <matthew.mcarthur91@gmail.com>
2018-02-08 18:10:31 -0400

    age counter with up/down logic

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: "Sean",
-    job: "ninja",
-    website: "mattwritesco.de",
-    websiteTag:'<a href="mattwritesco.de">the matt website website</a>'
-  },
+    age: 25,
+    },
   methods: {
-    greet: function(time){
-      return 'Good ' + time + ' ' + this.name;
-    }
   }
 });
