a9451c225820c49827bef5066740b772e64c968e
matthew <matthew.mcarthur91@gmail.com>
2018-02-08 18:34:10 -0400

    with canvas mouse tracker

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ new Vue({
   el: '#vue-app',
   data: {
     age: 25,
-    },
+    x: 0,
+    y: 0
+  },
   methods: {
-    add: function(){
-      this.age++;
+    add: function(x){
+      this.age += x;
     },
-    subtract function(){
-      this.age--;
+    subtract: function(x){
+      this.age -= x;
+    },
+    updateXY: function(event){
+      console.log(event);
+      this.x = event.clientX;
+      this.y = event.clientY;
     }
   }
 });
